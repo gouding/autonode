8ceb05a987f3eb20932c6270db6c7ef151fece38
denzel <422293027@qq.com>
2019-03-17 22:23:49 +0800

    dev

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const Koa = require('koa')
+const router = require('koa-router')
+const app = new Koa()
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain;charset=utf-8');
-  res.end('您好， 我是 大白--------55555');
-});
+app.use(router)
+router.get('/', (ctx, next) => {
+  ctx.response.body = '<h1>home页面</h1>'
+})
+app.listen(3000, () => {
+  console.log('running')
+})
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain;charset=utf-8');
+//   res.end('您好， 我是 大白--------55555');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
 
